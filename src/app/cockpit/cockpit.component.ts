32f624f17de4d599441dafb76317ab62f355a1b7
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-03-20 23:25:11 -0400

    Local access reference via viewchild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,24 +9,23 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string,serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string,serverContent: string}>();
   //newServerName = '';
-  newServerContent = '';
-
+  //newServerContent = '';
+  @ViewChild('serverContentInput',{static:true}) serverContentInput:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 onAddServer(nameInput: HTMLInputElement){
-  console.log(nameInput.value);
   this.serverCreated.emit({
     serverName: nameInput.value, 
-    serverContent: this.newServerContent
+    serverContent: this.serverContentInput.nativeElement.value
   });
 }
 
 onAddBlueprint(nameInput: HTMLInputElement){
   this.blueprintCreated.emit({
     serverName: nameInput.value, 
-    serverContent: this.newServerContent
+    serverContent: this.serverContentInput.nativeElement.value
   });
 }
 
